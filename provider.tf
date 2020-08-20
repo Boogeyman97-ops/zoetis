f9c27e183333087026564b7fb3b6f286c25ba55a
Boogeyman97-ops <67091478+Boogeyman97-ops@users.noreply.github.com>
2020-08-20 08:17:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,7 @@
 provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+  subscription_id = "abb119be-c8dd-42be-9b60-64269a565c61"
+  client_id       = "a20d5aa9-3048-40db-9f4c-a9ca299682cd"
+  client_secret   = "602.UsePMu9clcH4oT~V4ho54Z2L.NUNP."
+  tenant_id       = "426008e7-7e3f-4ad1-ba2d-7fd1e5c4d3b2"
 }
 
-variable "subscription_id" {
-  description = "Enter Subscription ID for provisioning resources in Azure"
-}
-
-variable "client_id" {
-  description = "Enter Client ID for Application created in Azure AD"
-}
-
-variable "client_secret" {
-  description = "Enter Client secret for Application in Azure AD"
-}
-
-variable "tenant_id" {
-  description = "Enter Tenant ID / Directory ID of your Azure AD. Run Get-AzureSubscription to know your Tenant ID"
-}
