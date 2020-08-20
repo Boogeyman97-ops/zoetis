bebccb035cd486a31b94722db7f278cf482884be
Boogeyman97-ops <67091478+Boogeyman97-ops@users.noreply.github.com>
2020-08-20 08:06:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,81 @@
-provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+resource "azurerm_resource_group" "resource_gp" {
+  name     = "Skylines-Demo-6"
+  location = "eastus"
+
+  tags {
+    Owner = "Aakash Kumar"
+  }
 }
 
-variable "subscription_id" {
-  description = "Enter Subscription ID for provisioning resources in Azure"
+variable "prefix" {
+  default = "sl"
 }
 
-variable "client_id" {
-  description = "Enter Client ID for Application created in Azure AD"
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.resource_gp.location}"
+  resource_group_name = "${azurerm_resource_group.resource_gp.name}"
 }
 
-variable "client_secret" {
-  description = "Enter Client secret for Application in Azure AD"
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "${azurerm_resource_group.resource_gp.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.2.0/24"
 }
 
-variable "tenant_id" {
-  description = "Enter Tenant ID / Directory ID of your Azure AD. Run Get-AzureSubscription to know your Tenant ID"
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = "${azurerm_resource_group.resource_gp.location}"
+  resource_group_name = "${azurerm_resource_group.resource_gp.name}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = "${azurerm_resource_group.resource_gp.location}"
+  resource_group_name   = "${azurerm_resource_group.resource_gp.name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags {
+    Owner = "Aakash Kumar"
+  }
 }
