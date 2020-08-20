8ff676cc14d766a8cf9d3d4e644c50f9ee1d1815
Boogeyman97-ops <67091478+Boogeyman97-ops@users.noreply.github.com>
2020-08-20 08:25:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,5 +3,6 @@ provider "azurerm" {
   client_id       = "a20d5aa9-3048-40db-9f4c-a9ca299682cd"
   client_secret   = "602.UsePMu9clcH4oT~V4ho54Z2L.NUNP."
   tenant_id       = "426008e7-7e3f-4ad1-ba2d-7fd1e5c4d3b2"
+  features {}
 }
 
